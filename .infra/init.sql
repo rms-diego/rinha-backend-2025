9b362e9f73bbb0156b3742c75c8e77d6a2fd2c20
Diego Ramos <diegorbrito9@gmail.com>
2025-08-02 17:04:55 -0300

    chore: add new column

--- .infra/init.sql
+++ .infra/init.sql
@@ -1,7 +1,8 @@
 CREATE UNLOGGED TABLE payments (
   correlationId UUID PRIMARY KEY,
   amount DECIMAL NOT NULL,
-  requested_at TIMESTAMP NOT NULL
+  requested_at TIMESTAMP NOT NULL,
+  is_default_processor BOOLEAN
 );
 
 CREATE INDEX payments_requested_at ON payments (requested_at);
